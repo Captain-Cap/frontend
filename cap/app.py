8b9d9aa1e29c68757114a164d61e3065d1fa1f42
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-12 19:53:50 +0800

    refactor: del file

--- cap/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    name_project = 'Captain Cap'
-    return render_template('index.html', title_project=name_project)
