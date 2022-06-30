be807336653977485341c5abb2207c157452b81f
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-30 20:21:12 +0800

    feature: new handle

--- cap/api/balloons.py
+++ cap/api/balloons.py
@@ -36,10 +36,15 @@ class BalloonApi:
         response = httpx.delete(f'{self.url}/api/v1/balloons/{uid}')
         response.raise_for_status()
 
-    def update(self, balloon: BalloonModel):
+    def update(self, balloon: BalloonModel) -> None:
         json_balloon = orjson.dumps(balloon.dict())
         headers = {
             'Content-Type': 'application/json',
         }
         response = httpx.put(f'{self.url}/api/v1/balloons/', content=json_balloon, headers=headers)
         response.raise_for_status()
+
+    def get_balloons_by_name_project(self, name) -> list[BalloonModel]:
+        response = httpx.get(f'{self.url}/api/v1/balloons/{name}')
+        response.raise_for_status()
+        return [BalloonModel(**balloon) for balloon in response.json()]
